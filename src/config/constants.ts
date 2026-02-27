aff0b17adfb014afe2f1d5c043a873472467e831
Fedi Rebai <rebaii@hotmail.com>
2026-02-27 11:37:41 +0100

    aa

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 export const MAX_MESSAGES = 200;
-export const INDENT = '  ';
+export const INDENT = '';
 export const FLUSH_INTERVAL = 400;
 export const BUBBLE_SIDE_MARGIN = 1;
 export const MAX_READABLE_WIDTH = 200;
